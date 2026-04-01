cf75a6d00eff8ccb2d657e468f70c6f973fe79ab
bllnzr <88485102+bllnzr@users.noreply.github.com>
2026-04-01 21:57:35 +0100

    Remove alert for patient readiness check

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -9,7 +9,7 @@
     }
 
     function onReady(smart)  {
-      alert('Updated13');
+     
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
